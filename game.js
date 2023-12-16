cb81d488757495e53ac90b080020f123558815de
Lydia He <75390984+iydia@users.noreply.github.com>
2023-12-16 15:13:39 -0500

    init game

---
+++ game.js
@@ -0,0 +1,22 @@
+// Create a 3x3 board
+const boardSize = 5;
+let board = Array.from({ length: boardSize }, () => Array(boardSize).fill(0));
+
+// Example: Add a random tile (3 or 9) to the board
+function addRandomTile() {
+  // Your code to add a random tile goes here
+}
+
+// Example: Handle user input (e.g., arrow keys)
+document.addEventListener('keydown', function(event) {
+  // Your code to handle key presses goes here
+});
+
+// Example: Update the game state and UI
+function updateGame() {
+  // Your code to update the game state and UI goes here
+}
+
+// Initialize the game
+addRandomTile();
+updateGame();
